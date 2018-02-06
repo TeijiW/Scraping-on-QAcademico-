f6f19ee584f597d9c3004c902d0505f8ebff0f82
Teiji <35480877+TeijiW@users.noreply.github.com>
2018-02-06 00:28:40 -0300

    In Progress

--- Scraping QAcademico.py
+++ Scraping QAcademico.py
@@ -1,13 +1,13 @@
 import requests 
 from bs4 import BeautifulSoup
 
-url = "https://academico.ifmt.edu.br/qacademico/lib/validalogin.asp"
+url = "https://academico.ifmt.edu.br/qacademico/lib/validalogin.asp" #Url where i need post the login 
 login = ("<Registration Nº>", "<password>")
-session = requests.session()
-r = session.post(url, auth=login, verify=False)
-print (r.status_code)
+session = requests.session() #Creation of the session 
+r = session.post(url, auth=login, verify=False) #Using the POST method, i give the user and password
+print (r.status_code) #Checking if everything is okay
 
-url = "http://academico.ifmt.edu.br/qacademico/index.asp?t=2000"
-r = session.get(url, verify=False)
+url = "http://academico.ifmt.edu.br/qacademico/index.asp?t=2000" #Url of the homepage after login page 
+r = session.get(url, verify=False) #Using the GET method for take the HTML file, but he is about a error page
 soup = BeautifulSoup(r.text, "lxml")
 print(soup)
